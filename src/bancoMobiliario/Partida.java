336ea90a7ae6306a740f3e9e699758f99b5beee1
Gabriel-Ribeiro-Dias <51831189+Gabriel-Ribeiro-Dias@users.noreply.github.com>
2019-08-11 15:49:13 -0300

    Adicionando exceptions e modificando Status

--- src/bancoMobiliario/Partida.java
+++ src/bancoMobiliario/Partida.java
@@ -1,21 +1,31 @@
 package bancoMobiliario;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Partida{
 	
-	public static void main(String[] args) throws JogadorComACorEscolhidaExiteException {
+	
+	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
 		SisJogo sis = new SisJogo();
 		ArrayList<Propriedade> tabuleiro = sis.genetareBoard();
 
 		
 		while(true) {
-			System.out.printf("Digite o número de jogadores [2-8]:");
-			int numJogadores = leitor.nextInt();
+			int numJogadores = 0;
+			try {
+				System.out.printf("Digite o número de jogadores [2-8]:");
+				numJogadores = leitor.nextInt();
+			} catch (InputMismatchException e) {
+				// TODO Auto-generated catch block
+				System.err.printf("%nException: %s%n",e);
+				leitor.nextLine();
+				System.out.printf("Você precisa digitar inteiros. Profavor tente denovo.%n%n");
+			}
 			
-			if(numJogadores<2 || numJogadores > 8) {System.out.println("Número de jogadores invalido");
+			if(numJogadores<2) {System.out.println("Número de jogadores não pode ser menor que 2.");
 			}else {
 				for (int k=0; k<numJogadores; k++) {
 					String nome,cor;
@@ -25,13 +35,14 @@ public class Partida{
 					
 					System.out.printf("Digite a cor do peão do jogador "+(k+1)+" entre as opções seguintes:\n"
 							+ "[preto][branco][vermelho][verde][azul][amarelo][laranja][rosa]\n:");
-					cor = leitor.next();
+					cor = leitor.next().toLowerCase();
 					try {
-						sis.gravaJogador((new Jogador(nome,cor)));/**JOGADOR É ADICIONADO A LISTA DE JOGADORES*/
-						System.out.println("Jogador cadastrado com sucesso");
-					}catch (JogadorComACorEscolhidaExiteException erro){
-						System.out.println(erro.getMessage());
-						
+						sis.gravaJogador((new Jogador(nome,cor.toLowerCase())));/**JOGADOR É ADICIONADO A LISTA DE JOGADORES*/
+					}catch (JogadorComCorEscolhidaExisteException exception) {
+						// TODO Auto-generated catch block
+						System.err.printf("%nException: %s%n",exception);
+						System.out.println("");
+						k = k-1;
 					}
 				}
 				break;
@@ -46,12 +57,12 @@ public class Partida{
 				System.out.println("Jogo encerrado.");
 				break;
 			}
-			for(int i = 0; i<sis.getJogadores().size(); i++) {
-				Jogador e = sis.getJogadores().get(i);
+			for(int k = 0; k<sis.getJogadores().size(); k++) {
+				Jogador e = sis.getJogadores().get(k);
 				while(true) {
 					if(e.getDiasNaPrisao()==0){
 						System.out.printf("A jogada de "+e.getNome()+" ("+e.getCor()+") começou.\n"
-								+ "Comandos disponíveis: [jogar] [status][sair]\n" 
+								+ "Comandos disponíveis: [jogar][status][sair]\n" 
 								+ "Digite um comando:");
 						String opcao = leitor.next().toUpperCase();
 						
@@ -59,24 +70,26 @@ public class Partida{
 							int numDadoUm = e.lancaDado();
 							int numDadoDois = e.lancaDado();sis.getJogadores();
 							e.setPosicao(numDadoUm, numDadoDois);
-							if(e.getPosicao()==10) {
+							if(e.getPosicao()==30) {
 								/** 
-								 * JOGADOR NÃO REALIZA NENHUMA AÇÃO
-								 */
-								System.out.println("O jogador "+e.getNome()+" "+e.getCor()+" tirou "+numDadoUm+","+numDadoDois+" e o peão avançou para "
-										+ ""+e.getPosicao()+" "
-										+ "– Prisão como visitante.");
-								break;
-							}else if(e.getPosicao()==30) {
-								/** 
-								 * JOGADOR NÃO REALIZA NENHUMA AÇÃO E VAI PARA A PRISÃO
+								 * JOGADOR NÃO REALIZA NENHUMA AÇÃO E VAI PARA PRISÃO
 								 */
 								System.out.println("O jogador "+e.getNome()+" "+e.getCor()+" tirou "+numDadoUm+","+numDadoDois+" e o peão avançou para "
 										+ ""+e.getPosicao()+" "
 										+ "– Prisão por 3 rodadas.");
 								e.irParaPrisao();
+								break;
+							}else if(e.getPosicao()==10){
+								/** 
+								 * JOGADOR NÃO REALIZA NENHUMA AÇÃO E VAI PARA PRISÃO COMO VISITANTE
+								 */
+								System.out.println("O jogador "+e.getNome()+" "+e.getCor()+" tirou "+numDadoUm+","+numDadoDois+" e o peão avançou para "
+										+ ""+e.getPosicao()+" "
+										+ "– Prisão como visitante.");
+								break;
+									
 							}else if((e.getPosicao()!=0 && e.getPosicao()!=2)&&(e.getPosicao()!=12 && e.getPosicao()!=16)&&(e.getPosicao()!=18 && e.getPosicao()!=20)
-									&&(e.getPosicao()!=24 && e.getPosicao()!=27)&&(e.getPosicao()!=30 && e.getPosicao()!=37)){
+									&&(e.getPosicao()!=24 && e.getPosicao()!=27)&&(e.getPosicao()!=37)){
 								Propriedade propriedade = tabuleiro.get(e.getPosicao());
 								if(propriedade.existeDono()==true) {
 									System.out.println("O jogador "+e.getNome()+" "+e.getCor()+" tirou "+numDadoUm+", "+numDadoDois+" e o peão avançou para "
@@ -96,36 +109,39 @@ public class Partida{
 									+ ""+e.getPosicao()+" "
 									+ "– "+propriedade.getNome()+". O título da propriedade "+ e.getNome()+" está disponível por $"+propriedade.getPrecoCompra()+"\n"
 									+ e.getNome()+", você possui "+e.getCarteira()+".\n"
-									+ "Você deseja comprar "+propriedade.getNome()+" (Sim/Não)? "); 
+									+ "Você deseja comprar Av. Europa (Sim/Não)? "); 
 								String escolha = leitor.next().toUpperCase();
 								if(escolha.equals("NÃO") | escolha.equals("NAO")) {
 									break;
 								}else {
-									e.compraPropriedade(propriedade.getPrecoCompra(), propriedade);
-									propriedade.setDono(e);
-									break;
+									try {
+										e.compraPropriedade(propriedade.getPrecoCompra(), propriedade);
+										propriedade.setDono(e);
+										System.out.println("Compra efetuada.");
+										break;
+									}catch(DinheiroInsuficienteException exception) {
+										System.out.println(exception);
+										break;
+									}
 								}/**JOGADOR DEVE ESCOLHER A AÇÃO A SER REALIZADA DEPENDENDO DA DE PROPRIEDADE ONDE ESTÁ LOZALIZADO*/}
 							
-							
 						}else if (opcao.equals("STATUS")) {
+							System.out.println(e.getStatus(tabuleiro));	
 							
-							System.out.println(e.getStatus(propriedade));
-						}
-						
-						
-						else if (opcao.equals("SAIR")) {
+						}else if(opcao.equals("SAIR")){
 								System.out.printf("Você realmente quer sair (Sim/Não)?");
 								String escolha = leitor.next().toUpperCase();
 								if(escolha.equals("SIM") | escolha.equals("S")) {sis.removeJogador(e);break;/** JOGADOR SERÁ REMOVIDO DA LISTA DE JOGADORES*/
 								}else if(escolha.equals("NÃO") | escolha.equals("NAO")) {}/**JOGADOR DEVERA ESCOLHER AS OPÇÕES DE JOGO NOVAMENTE*/
+						}else {
+							System.out.println("Digite uma opção válida.\n");
 						}
-					}else {System.out.println(e.getNome()+" esta na prisão como visitante.");e.setDiasNaProsao(1); break;/**OS DIAS NA PRISÃO DO JOGADOR SÃO DECREMENTADOS EM 1*/}
+					}else {System.out.println(e.getNome()+"Esta na prisão como visitante.");e.setDiasNaProsao(1); break;/**OS DIAS NA PRISÃO DO JOGADOR SÃO DECREMENTADOS EM 1*/}
 							
 				}//fim while
 			
 			}
 			
 		}
-		leitor.close();
 	}
 }
